eff45aa28ee260763b9bfe0bf652b941a6c89c5f
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-06-20 23:35:19 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,6 @@ git clone https://android.googlesource.com/toolchain/llvm
 sudo apt-get install gcc-arm-linux-gnueabi
 mkdir build
 cd build
-cmake ../llvm -DCMAKE_SYSTEM_NAME=arm-linux-gnueabi -DCMAKE_INSTALL_PREFIX=/system -DLLVM_HOST_TRIPLE=arm-linux-gnueabi
+cmake ../llvm -DCMAKE_SYSTEM_NAME=arm-linux-gnueabihf -DCMAKE_INSTALL_PREFIX=/system -DLLVM_HOST_TRIPLE=arm-linux-gnueabihf
 make -j128
 tar -c * | gzip -9 > llvm-sysroot.tar.gz
